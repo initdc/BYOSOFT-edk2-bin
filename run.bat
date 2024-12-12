3dd0f2a0f7ae00d0c9fa137a6fc3c43e2f3c251f
Wenchao Chen <chenwenchao@byosoft.com.cn>
2024-12-12 13:20:42 +0800

    add FD and Run.bat： RISCV_VIRT_CODE.fd、RISCV_VIRT_VARS.fd、run.bat
    Signed-off-by: Wenchao Chen <chenwenchao@byosoft.com.cn>

---
+++ run.bat
@@ -0,0 +1,17 @@
+@echo off
+REM QEMU RISC-V 64-bit virtual machine setup with GPU and USB devices
+REM This batch file sets up a QEMU virtual machine with RISC-V 64-bit architecture.
+
+qemu-system-riscv64 -M virt,pflash0=pflash0,pflash1=pflash1,acpi=off ^
+-m 4096 ^
+-smp 2 ^
+-device virtio-gpu-pci ^
+-device qemu-xhci ^
+-device usb-kbd ^
+-device usb-mouse ^
+-blockdev node-name=pflash0,driver=file,read-only=on,filename=RISCV_VIRT_CODE.fd ^
+-blockdev node-name=pflash1,driver=file,filename=RISCV_VIRT_VARS.fd ^
+-serial stdio ^
+-netdev tap,id=net0,ifname=tap0,script=no,downscript=no ^
+-device virtio-net-pci,netdev=net0 ^
+-device virtio-rng-pci ^
